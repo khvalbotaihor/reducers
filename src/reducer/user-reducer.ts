604e3ddf1d97f86da90b591f4057708b61726111
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-03-28 08:55:35 +0300

    - refactor state update

--- src/reducer/user-reducer.ts
+++ src/reducer/user-reducer.ts
@@ -12,11 +12,14 @@ type actionType = {
 export const userReducer = (state: StateType, action: actionType) =>{
     switch (action.type){
         case 'INCREMENT-AGE':
-            state.age = state.age + 1;
-            return state;
+            let newState = {...state}
+            newState.age = state.age + 1;
+            return newState;
         case 'INCREMENT-CHILDREN-COUNT':
-            state.childrenCount = state.childrenCount + 1;
-            return state;
+            return {
+                ...state,
+                childrenCount: state.childrenCount + 1
+            };
         default:
             throw new Error(`I don't understand this action type`)
     }
