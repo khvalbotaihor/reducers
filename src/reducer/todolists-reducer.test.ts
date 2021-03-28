b7824f4612cce838bbd6a0c17f87b4b05af70341
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-03-28 15:44:44 +0300

    - created unit tests to cover deleting, adding and updating item name

--- src/reducer/todolists-reducer.test.ts
+++ src/reducer/todolists-reducer.test.ts
@@ -15,3 +15,34 @@ test('correct todoList should be removed', () => {
     expect(endState.length).toBe(1)
     expect(endState[0].id).toBe(todoListId2)
 })
+
+test('new todoList should be added', () => {
+    let todoListId1 = v1();
+    let todoListId2 = v1();
+
+    const startState: Array<TodolistType> = [
+        {id: todoListId1, title: 'What to learn', filter: 'all'},
+        {id: todoListId2, title: 'What to buy', filter: 'all'}
+    ]
+
+    let newTodoListTitle = "New TodoList"
+
+    const endState = todoListReducer(startState, {type: 'ADD-TODOLIST', title:newTodoListTitle})
+    expect(endState.length).toBe(3)
+    expect(endState[2].title).toBe(newTodoListTitle)
+})
+test('correct todoList should change its name', () => {
+    let todoListId1 = v1();
+    let todoListId2 = v1();
+
+    const startState: Array<TodolistType> = [
+        {id: todoListId1, title: 'What to learn', filter: 'all'},
+        {id: todoListId2, title: 'What to buy', filter: 'all'}
+    ]
+
+    let newTodoListTitle = "New TodoList"
+
+    const endState = todoListReducer(startState, {type: 'CHANGE-TODOLIST-NAME', id:todoListId1, title:newTodoListTitle})
+    expect(endState[0].title).toBe(newTodoListTitle)
+    expect(endState[1].title).toBe("What to buy")
+})
--- src/reducer/todolists-reducer.ts
+++ src/reducer/todolists-reducer.ts
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {TodolistType} from "../App";
+import {v1} from "uuid";
 
 type actionType = {
     type: string
@@ -10,6 +11,17 @@ export const todoListReducer = (state: TodolistType[], action: actionType) => {
     switch (action.type){
         case 'DELETE-TODOLIST':
             return state.filter(tl => tl.id !== action.id)
+        case 'ADD-TODOLIST':
+            return [
+                ...state,
+                {id:v1(), title: action.title, filter: "all"}
+            ]
+        case 'CHANGE-TODOLIST-NAME':
+            let todoList = state.find(tl => tl.id=== action.id)
+            if(todoList){
+                todoList.title=action.title
+            }
+            return [...state]
         default:
             throw new Error(`I don't understand this action type`)
     }
